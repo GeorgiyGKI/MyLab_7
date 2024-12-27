1b6bb5cc7329be174f1a2873c8a0a76fb3461d86
GeorgiyGKI <138320367+GeorgiyGKI@users.noreply.github.com>
2024-12-27 07:32:21 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.lab_7"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lab_7"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11  // You can change this to your desired Java version (e.g., VERSION_8)
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+tasks.register("prepareKotlinBuildScriptModel") {
+
+}
+
+dependencies {
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.media)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
